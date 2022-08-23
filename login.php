965ace0d7f01a748e0391525f1063922b02d228b
PB2 <nft192891@gmail.com>
2022-08-23 22:15:33 +0800

    登入頁面

---
+++ login.php
@@ -0,0 +1,17 @@
+<div>
+
+<form action="./api/chk_login.php" method="POST">
+    <table>
+        <tr>
+            <td><input type="text" name="acc" placeholder="輸入帳號"></td>
+        </tr>
+        <tr>
+            <td><input type="text" name="pw" placeholder="輸入密碼"></td>
+        </tr>
+    </table>
+
+    <div>
+        <input class="log" type="submit" value="登 入">
+    </div>
+</form>
+</div>
